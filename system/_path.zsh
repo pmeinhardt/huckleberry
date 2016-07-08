e3f6df033727d3e71620367788ea16cb0041442a
pmeinhardt <mnhrdt@gmail.com>
2016-07-08 22:28:16 +0200

    Add system-wide aliases, path config, env config

---
+++ system/_path.zsh
@@ -0,0 +1,2 @@
+export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export MANPATH="/usr/local/share/man:$MANPATH"
---
+++ system/aliases.zsh
@@ -0,0 +1,4 @@
+alias ls="ls -G"
+alias ll="ls -la"
+alias la="ls -A"
+alias l="ls -1"
