0cda0ed933b0068c27bf4c9a31434ebf54a4d83a
pmeinhardt <mnhrdt@gmail.com>
2016-07-09 12:09:09 +0200

    Install shairport and its dependencies

---
+++ shairport/install.sh
@@ -0,0 +1,16 @@
+# install shairport from source
+
+if test $(which shairport); then exit 0; fi
+
+cd ~/local/src
+
+if [ ! -d shairport ]
+then
+  git clone https://github.com/abrasive/shairport.git
+fi
+
+cd shairport
+
+./configure && make
+
+cp shairport ~/local/bin
